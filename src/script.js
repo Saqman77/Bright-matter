6c107f4d5e2483af9c6041ca628cf86e43cd643b
Saqman77 <149172644+Saqman77@users.noreply.github.com>
2024-07-31 02:29:26 +0500

    b

--- src/script.js
+++ src/script.js
@@ -69,7 +69,7 @@ const parameters = {
 
 // Create an array for workers
 const workers = [];
-const workerCount = 1; // Number of workers
+const workerCount = 2; // Number of workers
 
 
 let xyz = {}
@@ -291,7 +291,7 @@ const debounce = (func, delay) => {
 //     }
 // };
 
-const debounceGenerateGalaxy = debounce(generateGalaxy, 0.05);
+const debounceGenerateGalaxy = debounce(generateGalaxy, 0.0);
 gsap.defaults({preventOverlaps:true,
     fastScrollEnd:true
 })
