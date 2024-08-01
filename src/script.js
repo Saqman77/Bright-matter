a0bf79ed5a3324bd172d481c52cf42d25e9f1cf8
Saqman77 <149172644+Saqman77@users.noreply.github.com>
2024-08-02 00:00:52 +0500

    b

--- src/script.js
+++ src/script.js
@@ -354,7 +354,7 @@ const debounce = (func, delay) => {
 //     }
 // };
 
-const debounceGenerateGalaxy = debounce(generateGalaxy, 0.05);
+const debounceGenerateGalaxy = debounce(generateGalaxy, 1);
 gsap.defaults({preventOverlaps:true,
     fastScrollEnd:true
 })
